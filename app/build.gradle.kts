a82140e9eaf4bde2ee8c0e498a2b60bc7b4d865c
wxxsfxyzm <65166044+wxxsfxyzm@users.noreply.github.com>
2026-09-05 11:28:19 +0800

    chore: bump version to 0.11.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
         minSdk = 36
         targetSdk = 37
         versionCode = gitVersionCode.get()
-        versionName = "0.11.7"
+        versionName = "0.11.8"
 
         ndk {
             abiFilters += "arm64-v8a"
